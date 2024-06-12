82f48c8cf1063b2db52546af13efba2e24b89277
ElenaCha <159483417+ElenaCha@users.noreply.github.com>
2024-06-12 10:18:29 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,6 +1,5 @@
 import configuration
 import requests
-import data
 
 
 # Создание заказа
@@ -16,17 +15,4 @@ def get_order(track_number):
     return response
 
 
-# Автотест
-def test_order_creation_and_retrieval():
-    response = create_order(data.order_body)
 
-    track_number = response.json()["track"]
-    print("Заказ создан. Номер трека:", track_number)
-
-    # Получение данных заказа по треку
-    order_response = get_order(track_number)
-
-    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
-    order_data = order_response.json()
-    print("Данные заказа:")
-    print(order_data)
